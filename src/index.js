dfdea597836317c3e7216fb5dd8f57d12fe32876
transcribemezub1-ctrl <transcribemezub1@gmail.com>
2026-01-27 20:41:25 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HockeyTeamBalancerWithUpload from './BH_Team_Balancer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HockeyTeamBalancerWithUpload />
+  </React.StrictMode>
+);
